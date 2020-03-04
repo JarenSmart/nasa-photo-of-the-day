24031ee047d8055f5a30bf6c5bae65fd97eb4f3b
jaren <jarensmart@yahoo.com>
2020-03-03 21:46:44 -0500

    added axios dependency

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [photo, setPhoto] = useState([]);
+  console.log(photo);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=Ta48wI2mWq1NVhBcRLhYHqCIheBSB9CWLc2yzSSu"
+      )
+      .then(response => {
+        setPhoto(response.data);
+        console.log(response.data);
+      })
+      .catch(error => console.log(error));
+  }, []);
+
   return (
     <div className="App">
       <p>
