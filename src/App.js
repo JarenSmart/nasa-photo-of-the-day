e34041579d7008ce75a853bd9651b6c7cd47515d
jaren <jarensmart@yahoo.com>
2020-03-05 19:23:34 -0500

    Fixed displaying data

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
-import axios from "axios";
+import PhotoCard from "./Components/PhotoCard";
 
 function App() {
-  const [photo, setPhoto] = useState([]);
-  console.log(photo);
-
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.nasa.gov/planetary/apod?api_key=Ta48wI2mWq1NVhBcRLhYHqCIheBSB9CWLc2yzSSu"
-      )
-      .then(response => {
-        setPhoto(response.data);
-        console.log(response.data);
-      })
-      .catch(error => console.log(error));
-  }, []);
-
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <span>
+        <h1>NASA Highlight of the Day</h1>
+        <PhotoCard />
+      </span>
     </div>
   );
 }
